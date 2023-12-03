50c6ca3bfeda6097d69814132f1b977361019df6
pavan <siriyalapavankumar@gmail.com>
2023-12-03 19:58:41 +0530

    class-1

---
+++ CLASS-1/script.js
@@ -0,0 +1,12 @@
+//   what i did today
+//    please refer to resouces and download ppt
+
+//    next programm on loop
+
+//    print  a program  that adds till 1 to 1000
+var print=1;
+for (  var i=1; i<1000; i++ ) {
+  var sum= print+i
+   console.log( print +  "+"  +i +"="  + sum);
+ 
+}
---
+++ operators.js
@@ -0,0 +1,35 @@
+// Problem 1: Write a JavaScript program to perform arithmetic operations on two numbers (addition, subtraction, multiplication, division).
+const number1 = prompt("enter  the number")
+const number2 = prompt("enter  the number")
+const addition = number1 + number2
+const subtraction = number1 - number2
+const multiplication = number1 * number2
+const division = number1 / number2
+ console.log(addition, ","+ multiplication,division,subtraction);
+
+
+// Problem 2: Write a JavaScript program to demonstrate the use of assignment operators on variables.
+
+
+// Problem 3: Write a JavaScript program to compare two numbers and log whether they are equal, greater, or lesser.
+
+
+// Problem 4: Write a JavaScript program to demonstrate logical operators in a conditional statement.
+
+
+// Problem 5: Write a JavaScript program to perform bitwise operations on two numbers.
+
+
+// Problem 6: Write a JavaScript program to use the ternary operator to determine the larger of two numbers.
+
+
+// Problem 7: Write a JavaScript program to demonstrate type operators on different data types.
+
+
+// Problem 8: Write a JavaScript program to use string operators to concatenate two strings.
+
+
+// Problem 9: Write a JavaScript program to demonstrate the use of the typeof operator on different variables.
+
+
+// Problem 10: Write a JavaScript program to use the in operator to check if a property exists in an object.
