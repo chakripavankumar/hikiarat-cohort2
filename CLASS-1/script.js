cd140e02b773a93724b648419e6c31cc2d09fb14
pavan <siriyalapavankumar@gmail.com>
2023-12-03 20:27:00 +0530

    class-1

--- CLASS-1/script.js
+++ CLASS-1/script.js
@@ -4,9 +4,11 @@
 //    next programm on loop
 
 //    print  a program  that adds till 1 to 1000
-var print=1;
-for (  var i=1; i<1000; i++ ) {
-  var sum= print+i
-   console.log( print +  "+"  +i +"="  + sum);
- 
-}
+//  second programm on loop & array
+//  wap to print even numbers
+// const arr = [23, 24, 40, 98, 67, 87, 97];
+// for (let i = 0; i <= arr.length; i++){
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i]);
+//     }
+// }
